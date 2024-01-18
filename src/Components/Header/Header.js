4acc1e4c370680ecb6faddf5393b75d2321d0d96
Roger Zavala <rogeliozavala15@gmail.com>
2024-01-18 11:56:08 -0800

    header

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,16 @@
+import './Header.css';
+
+const Header = () => {
+  return (
+    <header>
+      <img 
+        class="header-logo"
+        src="https://assets-global.website-files.com/63eeb6755510a42d663dba4c/63f11e60466e62711f4d73dd_logo-white.png" 
+        loading="eager" 
+        alt="stackline-logo" 
+      />
+    </header>
+  )
+}
+
+export default Header;
